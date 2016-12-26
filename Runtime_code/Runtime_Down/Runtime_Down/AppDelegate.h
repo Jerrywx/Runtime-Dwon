7625489619677fc7f0a6c725f9c7f4a27b364bc1
Jerrywx <23601145@qq.com>
2016-12-26 18:16:03 +0800

    创建runtime项目

---
+++ Runtime_code/Runtime_Down/Runtime_Down/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Runtime_Down
+//
+//  Created by 王潇 on 2016/12/26.
+//  Copyright © 2016年 王潇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Runtime_code/Runtime_Down/Runtime_Down/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Runtime_Down
+//
+//  Created by 王潇 on 2016/12/26.
+//  Copyright © 2016年 王潇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
