f4464e043e5e6186ecc3f67a07ea92f17268478f
Jerrywx <23601145@qq.com>
2016-12-28 19:05:08 +0800

    提交代码

---
+++ Runtime_code/Runtime_Down/Runtime_Down/Classes/MainClasses/WorkingwithClasses/ZHFPostHeaderView.h
@@ -0,0 +1,13 @@
+//
+//  ZHFPostHeaderView.h
+//  ZHCircle
+//
+//  Created by 王潇 on 2016/12/27.
+//  Copyright © 2016年 王潇. All rights reserved.
+//
+
+#import "ZHCView.h"
+
+@interface ZHFPostHeaderView : ZHCView
+
+@end
---
+++ Runtime_code/Runtime_Down/Runtime_Down/Classes/MainClasses/WorkingwithClasses/ZHFPostHeaderView.m
@@ -0,0 +1,21 @@
+//
+//  ZHFPostHeaderView.m
+//  ZHCircle
+//
+//  Created by 王潇 on 2016/12/27.
+//  Copyright © 2016年 王潇. All rights reserved.
+//
+
+#import "ZHFPostHeaderView.h"
+
+@implementation ZHFPostHeaderView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
